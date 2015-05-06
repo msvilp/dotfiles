8a57767953160475441f83aa93020fc42eea46f7
Mikko Vilpponen <vilpponen@protecomp.fi>
2015-05-06 12:43:02 +0300

    Added yaml-mode and lisp-dir for loading elisp files

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,3 +1,4 @@
+(add-to-list 'load-path "~/.emacs.d/lisp/")
 (require 'package)
 (add-to-list 'package-archives
              '("melpa-stable" . "http://melpa.org/packages/") t)
@@ -5,3 +6,6 @@
   ;; For important compatibility libraries like cl-lib
   (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
 (package-initialize)
+(require 'yaml-mode)
+(add-to-list 'auto-mode-alist '("\\.yml$" . yaml-mode))
+(add-to-list 'auto-mode-alist '("\\.yaml$" . yaml-mode))
