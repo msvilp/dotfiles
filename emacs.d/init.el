31662b233d4771a4bd8f58f2b8d978f690764908
Mikko Vilpponen <vilpponen@protecomp.fi>
2015-07-30 02:09:54 +0300

    Added uniquify to emacs config

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -9,3 +9,7 @@
 (require 'yaml-mode)
 (add-to-list 'auto-mode-alist '("\\.yml$" . yaml-mode))
 (add-to-list 'auto-mode-alist '("\\.yaml$" . yaml-mode))
+(require 'uniquify) 
+(setq 
+  uniquify-buffer-name-style 'post-forward
+  uniquify-separator ":")
